7279db30f672ed5c20a21fa9ac5d887086d0252f
Joshua Pella <109481857+pell-a@users.noreply.github.com>
2026-04-29 16:56:12 +0100

    Automated Security Fix

--- main.tf
+++ main.tf
@@ -16,8 +16,8 @@ resource "aws_s3_bucket_ownership_controls" "insecure_bucket" {
 resource "aws_s3_bucket_public_access_block" "insecure_bucket" {
   bucket = aws_s3_bucket.insecure_bucket.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
